4ce759b500582e4b12fed9d141de70da22b1a072
Mukesh <mukeshbhattampb@gmail.com>
2024-04-16 17:27:32 +0545

    update show more for github leetcode

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -3,7 +3,9 @@ import './MyWork.css'
 import theme_img from '../../assets/theme_pattern.svg'
 import mywork_data from '../../assets/mywork_data.js'
 import arrow_icon from '../../assets/arrow_icon.svg'
+import { useState } from 'react'
 const MyWork = () => {
+    const [show, setShow] = useState(false)
     return (
         <div id='work' className='mywork'>
             <div className="mywork-title">
@@ -18,13 +20,26 @@ const MyWork = () => {
                 })}
             </div>
 
-            <div className="mywork-showmore">
-                <p>Show More</p>
-                <img src={arrow_icon} alt="" />
-            </div>
+            {show ?
+
+                <>
+                    <div onClick={() => setShow(false)} className="mywork-showmore">
+                        <p>Show Less</p>
+                        <img src={arrow_icon} alt="" />
+                    </div>
+                    <div className="show">
+
+                        <h2><a href='https://github.com/MukeshMW1' target='_blank'>Githhub:Projects Repository</a></h2>
+                        <h2><a href='https://leetcode.com/MukeshMW1/' target='_blank'>Leetcode :Leetcode Profile </a></h2>
+                    </div>
+                </> :
+                <div onClick={() => setShow(true)} className="mywork-showmore">
+                    <p>Show More</p>
+                    <img src={arrow_icon} alt="" />
+                </div>}
 
 
-        </div>
+        </div >
     )
 }
 

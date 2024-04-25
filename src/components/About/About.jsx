020dc490d761989d9ccbbdc27dd3196eb5029f5f
Mukesh <mukeshbhattampb@gmail.com>
2024-04-25 13:54:34 +0545

    node js and express js updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,6 +27,8 @@ const About = () => {
                         <div className="about-skill"><p>React JS</p><hr style={{ width: "60%" }} /></div>
                         <div className="about-skill"><p>Javascript</p><hr style={{ width: "80%" }} /></div>
                         <div className="about-skill"><p>Python</p><hr style={{ width: "70%" }} /></div>
+                        <div className="about-skill"><p>Node JS</p><hr style={{ width: "40%" }} /></div>
+                        <div className="about-skill"><p>Express JS</p><hr style={{ width: "30%" }} /></div>
 
                     </div>
                 </div>

1d201152f5b27e27bd03fd0a6a55ea57d5302324
Mukesh <mukeshbhattampb@gmail.com>
2024-04-25 13:43:16 +0545

    Introduced Animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
     return (
         <div id='home' className='hero'>
             <img src={profile_img} alt="" />
-            <h1><span>Hello I'm Mukesh Bhatta , </span> a developer with a spirit of rising to  the  top. </h1>
+            <h1><span>Hello I'm Mukesh Bhatta , </span><TypeAnimation sequence={["a Frontend Dev...", 1000, "a Backend Dev...", 1000, "an App  Dev...", 1000, "an AI Dev...", 1000]} wrapper="span" speed={50} repeat={Infinity} />  </h1>
             <p>I am a frontend developer from Nepal , with 3 years of self experience</p>
 
             <div className="hero-connect">

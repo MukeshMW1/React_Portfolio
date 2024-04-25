a11fc9dff88d117636c8f88c20952ff27dc961c1
Mukesh <mukeshbhattampb@gmail.com>
2024-04-25 14:54:46 +0545

    ch

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
                     return <div key={index} className="services-format">
                         <h3>{service.s_no}</h3>
                         <h2>{service.s_name}</h2>
-                        <p>{service.s_desc}</p>
+                        <p className='desc'>{service.s_desc}</p>
 
                         <div className="services-readmore">
                             <p>Read More</p>

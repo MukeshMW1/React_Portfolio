59b6b3dc138149399f722d94c9285a76b115ab5b
Mukesh <mukeshbhattampb@gmail.com>
2024-09-15 23:09:13 +0545

    Image upload

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/about_profile.svg'
+import profile_img from '../../assets/profl.jpg'
 
 const About = () => {
     return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../assets/profl.jpg'
 import { TypeAnimation } from 'react-type-animation';
 import filePath from '../../../public/Resume.pdf'
 
@@ -9,7 +9,7 @@ const Hero = () => {
 
     return (
         <div id='home' className='hero'>
-            <img src={profile_img} alt="" />
+            <img src={profile_img}  alt=""   />
             <h1><span>Hello I'm Mukesh Bhatta , </span><TypeAnimation sequence={["a Frontend Dev...", 1000, "a Backend Dev...", 1000, "an App  Dev...", 1000, "an AI Dev...", 1000]} wrapper="span" speed={50} repeat={Infinity} />  </h1>
             <p>I am a frontend developer from Nepal , with 3 years of self experience</p>
 
